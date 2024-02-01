d880a19ae6919c8d55753655fb4876ebf2ef5241
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2024-02-01 17:09:01 +0700

    #5: add instacard footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,17 +1,12 @@
-import { FaYoutube, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 import Logo from "../logo/Logo";
 
 const navigation = {
   social: [
     {
       name: "Youtube",
-      href: "https://www.youtube.com/channel/UCZSFuQ0eoDxe8WXbt_th9zA",
+      href: "https://www.youtube.com/@saigon.digital",
       icon: (props) => <FaYoutube />,
-    },
-    {
-      name: "Twitter",
-      href: "https://twitter.com/lifteddotsite",
-      icon: (props) => <FaTwitter />,
     },
     {
       name: "GitHub",
@@ -29,8 +24,23 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="">
-      <Logo />
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Logo />
+        <div className="flex space-x-4">
+          {navigation.social.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              {item.icon({ className: "h-6 w-6" })}
+            </a>
+          ))}
+        </div>
+      </div>
     </footer>
   );
 }
